88339256254934d798aa691d6fa52f9c6fc38a59
jorgelucasalima <jorgelucasalima@gmail.com>
2021-10-15 14:55:07 -0300

    criado estrutura html do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-
+import {Container} from './styles'
 
 
 interface NewTransactionModalProps {
@@ -14,8 +14,18 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     <Modal 
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar Transação</h2>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input type="text" placeholder="Titulo"/>
+        <input type="number" placeholder="Valor"/>
+        <input type="text" placeholder="Categoria"/>
+        <button type="submit">Cadastrar</button>
+      </Container>
+
     </Modal>
   )
 
