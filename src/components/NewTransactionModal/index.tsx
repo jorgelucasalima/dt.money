f3ca5f92bb0104551d1cc8e1a25b4ba2b03616cb
jorgelucasalima <jorgelucasalima@gmail.com>
2021-10-18 21:46:24 -0300

    criado component NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import close from '../../assets/close.svg'
-import {Container} from './styles'
+import {Container,TransactionTypeContainer} from './styles'
+import income from '../../assets/income.svg'
+import outcome from '../../assets/outcome.svg'
 
 
 interface NewTransactionModalProps {
@@ -30,6 +32,24 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
         <input type="text" placeholder="Titulo"/>
         <input type="number" placeholder="Valor"/>
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={income} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcome} alt="Saída" />
+            <span>Saída</span>
+          </button>
+
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria"/>
         <button type="submit">Cadastrar</button>
       </Container>
