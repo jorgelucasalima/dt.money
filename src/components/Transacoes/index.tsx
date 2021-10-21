779aa9a51109f3927d5f489c4e23273e08cd936b
jorgelucasalima <jorgelucasalima@gmail.com>
2021-10-21 20:10:08 -0300

    formatando dados com Intl

--- src/components/Transacoes/index.tsx
+++ src/components/Transacoes/index.tsx
@@ -40,10 +40,18 @@ export function Transacoes() {
             {transactions.map(transaction => (
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>{transaction.amount}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)}
+                </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
-                <td></td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(
+                    new Date(transaction.createdAt)
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
